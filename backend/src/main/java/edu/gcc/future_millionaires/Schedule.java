71539755eed291fc62fced29b5fe2e58811477d5
CODDINGTONKM23 <coddingtonkm23@gcc.edu>
2026-02-23 13:46:58 -0500

    Pseudocoded schedule class. I think we should change the way we approach the schedule array. There is no reason to do Strings. At worst case scenario we could store course IDs but it makes so much more sense to just do a list of course objects. Also, I added a few extra methods. I think the bulk of the work for this is just going to be error handling with conflicting courses, this turned out being easier than I expected.

--- backend/src/main/java/edu/gcc/future_millionaires/Schedule.java
+++ backend/src/main/java/edu/gcc/future_millionaires/Schedule.java
@@ -1,4 +1,4 @@
-//package edu.gcc.comp350;
+package edu.gcc.future_millionaires;
 
 
 public class Schedule {
@@ -6,23 +6,53 @@ public class Schedule {
     // private class variables
     private int studentID;
     private String semester;
-    private String[] schedule;
+    private String[] schedule; //We should probably do a list of course objects instead, right?
     private int credits;
     private boolean overlap;
 
     // class methods
 
     // Constructor
-    public Schedule() {
-
+    public Schedule(int studentID, String semester) {
+        this.studentID = studentID;
+        this.semester = semester;
+        //this.schedule = new ArrayList<>();
+        this.credits = 0;
     }
 
     // Methods
     public void addCourse(String courseID) {
-
+//        if course is null BUT FIGURE OUT BEST WAY TO SEARCH BY ID
+//        inform user
+//        do not add
+//
+//        for each existing course in schedule
+//        if times conflict
+//        inform user
+//        do not add
+//
+//        add course object to list
+//        increase totalCredits
     }
 
     public void removeCourse(String courseID) {
-
+//        for each course in list
+//        if courseID matches
+//        remove it
+//        subtract credits
+//        return
+//
+//        notify user could not find course
     }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void clearSchedule() {
+        //schedule.clear(); <- did it as if it'll be a list
+        credits = 0;
+    }
+
+    //get scehdule method too?
 }

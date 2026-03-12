1846f07d80ce303fedfae42a05c95f5047ebf967
STANTONWJ23 <stantonWJ23@gcc.edu>
2026-03-11 23:13:01 -0400

    Finished Filter and Course classes. Wrote lots of JUnit tests in FilterTest.java

--- backend/src/main/java/edu/gcc/future_millionaires/Course.java
+++ backend/src/main/java/edu/gcc/future_millionaires/Course.java
@@ -25,9 +25,8 @@ public class Course {
     private String section;
     private String semester;
     private String subject;
-    private String department;
 
-    private List<TimeSlot> times;
+    private List<TimeSlot> times; // [{"day":"T","end_time":"16:45:00","start_time":"15:30:00"},{"day":"R","end_time":"16:45:00","start_time":"15:30:00"}]
 
     @JsonProperty("total_seats")
     private int totalSeats;
@@ -141,119 +140,5 @@ public class Course {
         this.totalSeats = totalSeats;
     }
 
-    public String getDepartment() { return department; }
 
-
-    // old file:
-//    public class Course {
-//
-//        // private class variables
-//        private int courseID;
-//        private String department;
-//        private int courseCode;
-//        private String professor;
-//        private String[] days;
-//        private String semester;
-//        private int credits;
-//        private List<LocalTime[]> meetingTime;
-//        private int maxCapacity;
-//        private ArrayList<Integer> enrolledStudents;
-//
-//
-//        // class methods
-//
-//        // Constructor
-//        public Course() {
-//            enrolledStudents = new ArrayList<>();
-//        }
-//        // getters
-//        public int getCourseID() {
-//            return courseID;
-//        }
-//        public String getDepartment() {
-//            return department;
-//        }
-//        public int getCourseCode() {
-//            return courseCode;
-//        }
-//        public String getProfessor() {
-//            return professor;
-//        }
-//        public String[] getDays() {
-//            return days;
-//        }
-//        public String getSemester() {
-//            return semester;
-//        }
-//        public int getCredits() {
-//            return credits;
-//        }
-//        public List<LocalTime[]>  getMeetingTime() {
-//            return meetingTime;
-//        }
-//        public int getMaxCapacity() {
-//            return maxCapacity;
-//        }
-//        public ArrayList<Integer> getEnrolledStudents() {
-//            return enrolledStudents;
-//        }
-//
-//        // setters
-//        public void setCourseID(int courseID) {
-//            this.courseID = courseID;
-//        }
-//        public void setDepartment(String department) {
-//            this.department = department;
-//        }
-//        public void setCourseCode(int courseCode) {
-//            this.courseCode = courseCode;
-//        }
-//        public void setProfessor(String professor) {
-//            this.professor = professor;
-//        }
-//        public void setDays(String[] days) {
-//            this.days = days;
-//        }
-//        public void setSemester(String semester) {
-//            this.semester = semester;
-//        }
-//        public void setCredits(int credits) {
-//            this.credits = credits;
-//        }
-//        public void setMeetingTime(List<LocalTime[]> meetingTime) {
-//            this.meetingTime = meetingTime;
-//        }
-//        public void setMaxCapacity(int maxCapacity) {
-//            this.maxCapacity = maxCapacity;
-//        }
-//        public void setEnrolledStudents(ArrayList<Integer> enrolledStudents) {
-//            this.enrolledStudents = enrolledStudents;
-//        }
-//
-//        // other methods
-//        public void deleteCourse() {
-//            enrolledStudents.clear(); // clear the list of students
-//        }
-//
-//        public void archiveCourse() {
-//
-//        }
-//
-//        public void editCourse(String professor, String[] days, List<LocalTime[]> meetingTime, int maxCapacity) {
-//            setProfessor(professor);
-//            setDays(days);
-//            setMeetingTime(meetingTime);
-//            setMaxCapacity(maxCapacity);
-//        }
-//
-//        public void addStudent(int studentID) {
-//            if (!enrolledStudents.contains(studentID) && enrolledStudents.size() < maxCapacity) {
-//                enrolledStudents.add(studentID);
-//            }
-//        }
-//
-//        public void removeStudent(int studentID) {
-//            enrolledStudents.remove(studentID);
-//        }
-//    }
 }

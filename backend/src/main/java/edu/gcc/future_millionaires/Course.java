bafe11c0a205b9dc3c7537e1bc129a2092b78388
MAYHEWCJ22 <cj4of7@icloud.com>
2026-04-20 17:08:53 -0400

    tiny bug fix

--- backend/src/main/java/edu/gcc/future_millionaires/Course.java
+++ backend/src/main/java/edu/gcc/future_millionaires/Course.java
@@ -1,8 +1,10 @@
 package edu.gcc.future_millionaires;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Course {
 
     private int credits;
